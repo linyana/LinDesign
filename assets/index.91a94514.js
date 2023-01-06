8c58241e3766b24715ce9b956347633a1f60ce46
LinYan <1439713475@qq.com>
2023-01-06 10:39:16 +0800

    build

---
+++ assets/index.91a94514.js
@@ -0,0 +1,224 @@
+import{S as i}from"./index.83921006.js";import{a as F,F as t,j as B}from"./index.30eb1a31.js";import{i as v}from"./img.18c9eb25.js";const r="/LinUI-page/assets/img2.c2802dee.jpg",n=()=>F(t,{children:[B("div",{className:"compontent_title",id:"content-anchor1",children:"\u56FE\u65871"}),F("div",{className:"component_box",children:[B("div",{className:"compontent_box_content1",children:F("div",{className:"component_content_content1",children:[B("img",{src:r,alt:"\u8BF7\u6DFB\u52A0\u56FE\u7247"}),F("div",{className:"text",children:[B("h2",{style:{color:"rgb(73,80,87)"},children:"\u5185\u5BB9"}),B("p",{style:{color:"rgb(73,80,87)"},children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002"})]})]})}),B(i,{code:{element:()=>F("div",{className:"component_content_content1",children:[B("img",{src:r,alt:"\u8BF7\u6DFB\u52A0\u56FE\u7247"}),F("div",{className:"text",children:[B("h2",{children:"\u5185\u5BB9"}),B("p",{children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002"})]})]}),HTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+  <img src="" alt="\u8BF7\u6DFB\u52A0\u56FE\u7247" />
+  <div class="text">
+    <h2>\u5185\u5BB9</h2>
+    <p>
+    \u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002
+    </p>
+  </div>
+</div>`,CSSCode:(u,d,e,o)=>`${u===""?"div":u} {
+  overflow: hidden;
+  margin: 20px 0;
+  padding: 0 30px;
+}
+
+${u===""?"div":u} img {
+  width: 40%;
+  float: left;
+}
+
+${u===""?"div":u} .text {
+  /* \u8FD9\u4E2A60px\u4E3Apadding\u7B2C\u4E8C\u4E2A\u503C\u7684\u53CC\u500D */
+  padding-left: 30px;
+  width: calc(60% - 30px);
+  float: left;
+}
+
+${u===""?"div":u} h2 {
+  margin-bottom: 10px;
+  color: ${e};
+}
+
+${u===""?"div":u} p {
+  text-indent: 2em;
+  line-height: 28px;
+  letter-spacing: 1px;
+  color: ${e};
+}`,JSCode:()=>"\u6682\u65E0",VueHTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+  <img src="" alt="\u8BF7\u6DFB\u52A0\u56FE\u7247" />
+  <div class="text">
+    <h2>\u5185\u5BB9</h2>
+    <p>
+    \u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002
+    </p>
+  </div>
+</div>`,VueJSCode:()=>"\u6682\u65E0",ReactHTMLCode:u=>`<div${u.trim()?` className="${u.replace(".","")}"`:""}>
+    <img src="" alt="\u8BF7\u6DFB\u52A0\u56FE\u7247" />
+    <div className="text">
+      <h2>\u5185\u5BB9</h2>
+      <p>
+      \u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002
+      </p>
+    </div>
+  </div>
+</div>`,ReactJSCode:()=>"\u6682\u65E0",TSCode:()=>"\u6682\u65E0",VueTSCode:()=>"\u6682\u65E0",ReactTSCode:()=>"\u6682\u65E0"}})]})]});const a=()=>F(t,{children:[B("div",{className:"compontent_title",id:"content-anchor2",children:"\u56FE\u65872"}),F("div",{className:"component_box",children:[B("div",{className:"compontent_box_content1",children:F("div",{className:"component_content_content2",children:[B("img",{src:v,alt:"\u8BF7\u6DFB\u52A0\u56FE\u7247"}),B("div",{className:"text",style:{backgroundColor:"rgb(224,184,120)"},children:F("div",{className:"text_content",children:[B("h2",{style:{color:"rgb(248, 248, 248)"},children:"\u5185\u5BB9"}),B("p",{style:{color:"rgb(248, 248, 248)"},children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002"})]})})]})}),B(i,{code:{element:()=>F("div",{className:"component_content_content2",children:[B("img",{src:v,alt:"\u8BF7\u6DFB\u52A0\u56FE\u7247"}),B("div",{className:"text",children:F("div",{className:"text_content",children:[B("h2",{children:"\u5185\u5BB9"}),B("p",{children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002"})]})})]}),HTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+  <img src="" alt="\u8BF7\u6DFB\u52A0\u56FE\u7247" />
+  <div class="text">
+    <div class="text_content">
+      <h2>\u5185\u5BB9</h2>
+      <p>
+        \u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002
+      </p>
+    </div>
+  </div>
+</div>`,CSSCode:(u,d,e,o)=>`${u===""?"div":u} {
+  position: relative;
+  margin: 20px 0;
+  padding-left: 30px;
+}
+
+${u===""?"div":u} img {
+  width: 70%;
+}
+
+${u===""?"div":u} .text {
+  position: absolute;
+  right: 5%;
+  top: 5%;
+  width: 50%;
+  height: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  overflow: hidden; 
+  background-color: ${d};
+}
+
+${u===""?"div":u} .text_content {
+  padding: 0 10%;
+}
+
+${u===""?"div":u} h2 {
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  color: ${e};
+}
+
+${u===""?"div":u} p {
+  text-indent: 2em;
+  line-height: 28px;
+  letter-spacing: 0.3px;
+  color: ${e};
+}`,JSCode:()=>"\u6682\u65E0",VueHTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+  <img src="" alt="\u8BF7\u6DFB\u52A0\u56FE\u7247" />
+  <div class="text">
+    <div class="text_content">
+      <h2>\u5185\u5BB9</h2>
+      <p>
+        \u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002
+      </p>
+    </div>
+  </div>
+</div>`,VueJSCode:()=>"\u6682\u65E0",ReactHTMLCode:u=>`<div${u.trim()?` className="${u.replace(".","")}"`:""}>
+    <img src="" alt="\u8BF7\u6DFB\u52A0\u56FE\u7247" />
+    <div className="text">
+      <div className="text_content">
+        <h2>\u5185\u5BB9</h2>
+        <p>
+          \u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u3002
+        </p>
+      </div>
+    </div>
+  </div>
+</div>`,ReactJSCode:()=>"\u6682\u65E0",TSCode:()=>"\u6682\u65E0",VueTSCode:()=>"\u6682\u65E0",ReactTSCode:()=>"\u6682\u65E0"}})]})]});const l=()=>F(t,{children:[B("div",{className:"compontent_title",id:"basic_button",children:"\u9F20\u6807\u7ECF\u8FC7\u53D8\u5BBD"}),B("p",{className:"compontent_p",children:"\u4E3A\u4E86\u9AD8\u5EA6\u4E0D\u4F1A\u968F\u7740\u5BBD\u5EA6\u800C\u6539\u53D8\uFF0C\u8BF7\u6839\u636E\u9700\u8981\u8BBE\u7F6E\u9AD8\u5EA6\u4E3A\u786E\u5B9A\u7684\u503C,\u672C\u7EC4\u4EF6\u4E0D\u652F\u6301\u81EA\u5B9A\u4E49\u989C\u8272\u3002"}),F("div",{className:"component_box",children:[B("div",{className:"compontent_box_content",children:F("div",{className:"card_component4_content",style:{width:700},children:[F("div",{className:"move_card move_card1",children:[B("div",{className:"move_card_title",children:"\u6807\u9898"}),B("div",{className:"move_card_text",children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9"})]}),F("div",{className:"move_card move_card2",children:[B("div",{className:"move_card_title",children:"\u6807\u9898"}),B("div",{className:"move_card_text",children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9"})]}),F("div",{className:"move_card move_card3",children:[B("div",{className:"move_card_title",children:"\u6807\u9898"}),B("div",{className:"move_card_text",children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9"})]})]})}),B(i,{code:{element:()=>F("div",{className:"card_component4_content",style:{width:700},children:[F("div",{className:"move_card move_card1",children:[B("div",{className:"move_card_title",children:"\u6807\u9898"}),B("div",{className:"move_card_text",children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9"})]}),F("div",{className:"move_card move_card2",children:[B("div",{className:"move_card_title",children:"\u6807\u9898"}),B("div",{className:"move_card_text",children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9"})]}),F("div",{className:"move_card move_card3",children:[B("div",{className:"move_card_title",children:"\u6807\u9898"}),B("div",{className:"move_card_text",children:"\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9"})]})]}),HTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+	<div class="move_card move_card1">
+		<div class="move_card_title">\u6807\u9898</div>
+		<div class="move_card_text">
+			\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9
+		</div>
+	</div>
+	<div class="move_card move_card2">
+		<div class="move_card_title">\u6807\u9898</div>
+		<div class="move_card_text">
+			\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9
+		</div>
+	</div>
+	<div class="move_card move_card3">
+		<div class="move_card_title">\u6807\u9898</div>
+		<div class="move_card_text">
+			\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9
+		</div>
+	</div>
+</div>`,CSSCode:(u,d,e,o)=>`${u===""?"div":u} {
+  display: flex;
+  justify-content: space-around;
+}
+
+${u===""?"div":u} .move_card {
+  width: 22%;
+  padding: 3%;
+  height: 200px;
+  transition: all 0.8s;
+}
+
+${u===""?"div":u} .move_card:hover {
+  width: 36%;
+}
+
+${u===""?"div":u} .move_card1 {
+  background-color: #e7b46c;
+}
+
+${u===""?"div":u} .move_card2 {
+  background-color: #6e9ae5;
+}
+
+${u===""?"div":u} .move_card3 {
+  background-color: #7277c3;
+}
+
+${u===""?"div":u} .move_card .move_card_title,
+${u===""?"div":u} .move_card .move_card_text {
+  color: white;
+}
+
+${u===""?"div":u} .move_card .move_card_title {
+  text-align: center;
+  font-size: 26px;
+  line-height: 60px;
+}
+
+${u===""?"div":u} .move_card .move_card_text {
+  width: 100%
+}
+        `,JSCode:()=>"\u6682\u65E0",VueHTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+	<div class="move_card move_card1">
+		<div class="move_card_title">\u6807\u9898</div>
+		<div class="move_card_text">
+			\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9
+		</div>
+	</div>
+	<div class="move_card move_card2">
+		<div class="move_card_title">\u6807\u9898</div>
+		<div class="move_card_text">
+			\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9
+		</div>
+	</div>
+	<div class="move_card move_card3">
+		<div class="move_card_title">\u6807\u9898</div>
+		<div class="move_card_text">
+			\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9
+		</div>
+	</div>
+</div>`,VueJSCode:()=>"\u6682\u65E0",ReactHTMLCode:u=>`<div${u.trim()?` className="${u.replace(".","")}"`:""}>
+	<div className="move_card move_card1">
+		<div className="move_card_title">\u6807\u9898</div>
+		<div className="move_card_text">
+			\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9
+		</div>
+	</div>
+	<div className="move_card move_card2">
+		<div className="move_card_title">\u6807\u9898</div>
+		<div className="move_card_text">
+			\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9
+		</div>
+	</div>
+	<div className="move_card move_card3">
+		<div className="move_card_title">\u6807\u9898</div>
+		<div className="move_card_text">
+			\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9\u6211\u662F\u5185\u5BB9
+		</div>
+	</div>
+</div>`,ReactJSCode:()=>"\u6682\u65E0",TSCode:()=>"\u6682\u65E0",VueTSCode:()=>"\u6682\u65E0",ReactTSCode:()=>"\u6682\u65E0"}})]})]}),h=()=>F("div",{children:[F("div",{className:"component_big_title",children:["\u5185\u5BB9 ",B("span",{children:"Content"})]}),B(n,{}),B(a,{}),B(l,{})]});export{h as default};
