05ca3761d616847be58f9f6f2b7961d86379c7b1
LinYan <1439713475@qq.com>
2023-01-29 09:59:43 +0800

    build

---
+++ assets/index.4fc8cb36.js
@@ -0,0 +1,317 @@
+import{S as e}from"./index.665e513b.js";import{w as F}from"./wrong.13f6fc9b.js";import{a as d,F as r,j as t}from"./index.b54d12af.js";/* empty css              */const l=()=>d(r,{children:[t("div",{className:"compontent_title",id:"button-anchor3",children:"\u57FA\u672C\u8868\u683C"}),t("p",{className:"compontent_p",children:"\u5BF9\u8868\u683C\u7684\u989C\u8272\u8FDB\u884C\u57FA\u672C\u7684\u5C01\u88C5,\u5982\u679C\u8868\u683C\u5185\u5BB9\u8FC7\u591A\uFF0C\u53EF\u4EE5\u7ED9\u6700\u5916\u5C42\u7684\u76D2\u5B50\u8BBE\u7F6Eoverflow:scroll\uFF0C\u7136\u540E\u53C2\u8003\u6EDA\u52A8\u6761\u8FDB\u884C\u4FEE\u9970\u3002"}),d("p",{className:"function_p",children:["\u80CC\u666F\u989C\u8272\uFF1A",t("img",{src:F,alt:""})]}),d("p",{className:"function_p",children:["\u5B57\u4F53\u989C\u8272\uFF1A",t("img",{src:F,alt:""})]}),d("p",{className:"function_p",children:["\u60AC\u6D6E\u989C\u8272\uFF1A",t("img",{src:F,alt:""})]}),d("div",{className:"component_box",children:[t("div",{className:"compontent_box_content",children:t("div",{className:"table_component1_content",children:d("table",{cellSpacing:0,children:[t("thead",{children:d("tr",{children:[t("td",{children:"\u5E8F\u53F7"}),t("td",{children:"\u59D3\u540D"}),t("td",{children:"\u6027\u522B"}),t("td",{children:"\u63CF\u8FF0"}),t("td",{children:"\u65E5\u671F"})]})}),d("tbody",{children:[d("tr",{children:[t("td",{children:"1"}),t("td",{children:"\u5C0F\u674E"}),t("td",{children:"\u7537"}),t("td",{children:"\u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0"}),t("td",{children:"2022/12/3"})]}),d("tr",{children:[t("td",{children:"2"}),t("td",{children:"\u5C0F\u674E"}),t("td",{children:"\u7537"}),t("td",{children:"\u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0"}),t("td",{children:"2022/12/3"})]}),d("tr",{children:[t("td",{children:"3"}),t("td",{children:"\u5C0F\u674E"}),t("td",{children:"\u7537"}),t("td",{children:"\u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0"}),t("td",{children:"2022/12/3"})]})]})]})})}),t(e,{code:{element:()=>t("div",{className:"layout_component1_content1",children:"content"}),HTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+  <table cellSpacing={0}>
+    <thead>
+      <tr>
+        <td>\u5E8F\u53F7</td>
+        <td>\u59D3\u540D</td>
+        <td>\u6027\u522B</td>
+        <td>
+          \u63CF\u8FF0
+        </td>
+        <td>\u65E5\u671F</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+    </tbody>
+  </table>
+</div>`,CSSCode:(u,i,o,c)=>` 
+${u===""?"div":u} {
+  width: 100%;
+}
+
+${u===""?"div":u} table td{
+  border: 1px solid rgb(132, 160, 173);
+  border-right: none;
+  border-bottom: none;
+  text-align: center;
+  padding: 4px 20px;
+}
+
+${u===""?"div":u} table td:last-child {
+  border-right: 1px solid rgb(132, 160, 173);
+}
+
+${u===""?"div":u} table tr:last-child td {
+  border-bottom: 1px solid rgb(132, 160, 173);
+}
+
+${u===""?"div":u} table thead td {
+  background-color: rgb(87, 153, 214);
+  color: white;
+  font-weight: 700;
+  font-size: 16px;
+}
+
+${u===""?"div":u} table tbody td {
+  background-color: rgb(187, 212, 235);
+  color: #495057;
+  font-size: 14px;
+}
+        `,JSCode:()=>"\u6682\u65E0",VueHTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+  <table cellSpacing={0}>
+    <thead>
+      <tr>
+        <td>\u5E8F\u53F7</td>
+        <td>\u59D3\u540D</td>
+        <td>\u6027\u522B</td>
+        <td>
+          \u63CF\u8FF0
+        </td>
+        <td>\u65E5\u671F</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+    </tbody>
+  </table>
+</div>`,VueJSCode:()=>"\u6682\u65E0",ReactHTMLCode:u=>`<div${u.trim()?` className="${u.replace(".","")}"`:""}>
+  <table cellSpacing={0}>
+    <thead>
+      <tr>
+        <td>\u5E8F\u53F7</td>
+        <td>\u59D3\u540D</td>
+        <td>\u6027\u522B</td>
+        <td>
+          \u63CF\u8FF0
+        </td>
+        <td>\u65E5\u671F</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+    </tbody>
+  </table>
+</div>`,ReactJSCode:()=>"\u6682\u65E0",TSCode:()=>"\u6682\u65E0",VueTSCode:()=>"\u6682\u65E0",ReactTSCode:()=>"\u6682\u65E0"}})]})]});const C=()=>d(r,{children:[t("div",{className:"compontent_title",id:"button-anchor3",children:"\u7CFB\u7EDF\u8868\u683C"}),d("p",{className:"function_p",children:["\u80CC\u666F\u989C\u8272\uFF1A",t("img",{src:F,alt:""})]}),d("p",{className:"function_p",children:["\u5B57\u4F53\u989C\u8272\uFF1A",t("img",{src:F,alt:""})]}),d("p",{className:"function_p",children:["\u60AC\u6D6E\u989C\u8272\uFF1A",t("img",{src:F,alt:""})]}),d("div",{className:"component_box",children:[t("div",{className:"compontent_box_content",children:t("div",{className:"table_component2_content",children:d("table",{cellSpacing:0,children:[t("thead",{children:d("tr",{children:[t("td",{children:"\u5E8F\u53F7"}),t("td",{children:"\u59D3\u540D"}),t("td",{children:"\u6027\u522B"}),t("td",{children:"\u63CF\u8FF0"}),t("td",{children:"\u65E5\u671F"})]})}),d("tbody",{children:[d("tr",{children:[t("td",{children:"1"}),t("td",{children:"\u5C0F\u674E"}),t("td",{children:"\u7537"}),t("td",{children:"\u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0"}),t("td",{children:"2022/12/3"})]}),d("tr",{children:[t("td",{children:"2"}),t("td",{children:"\u5C0F\u674E"}),t("td",{children:"\u7537"}),t("td",{children:"\u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0"}),t("td",{children:"2022/12/3"})]}),d("tr",{children:[t("td",{children:"3"}),t("td",{children:"\u5C0F\u674E"}),t("td",{children:"\u7537"}),t("td",{children:"\u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0"}),t("td",{children:"2022/12/3"})]})]})]})})}),t(e,{code:{element:()=>t("div",{className:"layout_component1_content1",children:"content"}),HTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+  <table cellSpacing={0}>
+    <thead>
+      <tr>
+        <td>\u5E8F\u53F7</td>
+        <td>\u59D3\u540D</td>
+        <td>\u6027\u522B</td>
+        <td>
+          \u63CF\u8FF0
+        </td>
+        <td>\u65E5\u671F</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+    </tbody>
+  </table>
+</div>`,CSSCode:(u,i,o,c)=>`
+${u===""?"div":u} {
+  width: 100%;
+}
+
+${u===""?"div":u} table td {
+  border-bottom: 1px solid rgb(168, 167, 167);
+  color: #495057;
+  padding: 8px 20px;
+  text-align: center;
+}
+
+${u===""?"div":u} table tbody tr:hover {
+  background-color: rgb(243, 243, 243);
+  transition: all 0.3s;
+}
+
+${u===""?"div":u} table thead td {
+  font-weight: 700;
+  font-size: 16px;
+}
+
+${u===""?"div":u} table tbody td {
+  font-size: 14px;
+}
+        `,JSCode:()=>"\u6682\u65E0",VueHTMLCode:u=>`<div${u.trim()?` class="${u.replace(".","")}"`:""}>
+  <table cellSpacing={0}>
+    <thead>
+      <tr>
+        <td>\u5E8F\u53F7</td>
+        <td>\u59D3\u540D</td>
+        <td>\u6027\u522B</td>
+        <td>
+          \u63CF\u8FF0
+        </td>
+        <td>\u65E5\u671F</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+    </tbody>
+  </table>
+</div>`,VueJSCode:()=>"\u6682\u65E0",ReactHTMLCode:u=>`<div${u.trim()?` className="${u.replace(".","")}"`:""}>
+  <table cellSpacing={0}>
+    <thead>
+      <tr>
+        <td>\u5E8F\u53F7</td>
+        <td>\u59D3\u540D</td>
+        <td>\u6027\u522B</td>
+        <td>
+          \u63CF\u8FF0
+        </td>
+        <td>\u65E5\u671F</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>\u5C0F\u674E</td>
+        <td>\u7537</td>
+        <td>
+          \u8FD9\u662F\u4E00\u6BB5\u5F88\u957F\u7684\u63CF\u8FF0
+        </td>
+        <td>2022/12/3</td>
+      </tr>
+    </tbody>
+  </table>
+</div>`,ReactJSCode:()=>"\u6682\u65E0",TSCode:()=>"\u6682\u65E0",VueTSCode:()=>"\u6682\u65E0",ReactTSCode:()=>"\u6682\u65E0"}})]})]}),B=()=>d("div",{children:[d("div",{className:"component_big_title",children:["\u8868\u683C ",t("span",{children:"Table"})]}),t(l,{}),t(C,{})]});export{B as default};
