8705e010e9afed0fbc8391d74fb48c66926401e8
LinYan <1439713475@qq.com>
2023-01-29 10:56:56 +0800

    build

---
+++ assets/index.4de8f649.js
@@ -0,0 +1,86 @@
+import{S as e}from"./index.ebb7d360.js";import{w as o}from"./wrong.13f6fc9b.js";import{r as n}from"./right.acc19ff3.js";import{a as l,F as s,j as c}from"./index.f89316bc.js";/* empty css              */const u=()=>l(s,{children:[c("div",{className:"compontent_title",id:"button-anchor3",children:"\u57FA\u672C\u4F18\u5316"}),c("p",{className:"compontent_p",children:"\u5BF9\u6EDA\u52A8\u6761\u7684\u6837\u5F0F\u8FDB\u884C\u4F18\u5316\uFF0C\u53EF\u4EE5\u5C06css\u4E2D\u7684\u7C7B\u9009\u62E9\u5668\u5220\u9664\u4EE5\u4F5C\u7528\u4E8E\u5168\u5C40\u3002"}),l("p",{className:"function_p",children:["\u80CC\u666F\u989C\u8272\uFF1A",c("img",{src:n,alt:""})]}),l("p",{className:"function_p",children:["\u5B57\u4F53\u989C\u8272\uFF1A",c("img",{src:o,alt:""})]}),l("p",{className:"function_p",children:["\u60AC\u6D6E\u989C\u8272\uFF1A",c("img",{src:o,alt:""})]}),l("div",{className:"component_box",children:[c("div",{className:"compontent_box_content",children:l("div",{className:"scrollBar_component1_content scrollBar_component1_content_color",children:[c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"})]})}),c(e,{code:{element:()=>l("div",{className:"scrollBar_component1_content",children:[c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"}),c("div",{className:"scrollBar_content",children:"\u5185\u5BB9"})]}),HTMLCode:r=>`<div${r.trim()?` class="${r.replace(".","")}"`:""}>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+</div>`,CSSCode:(r,i,a,d)=>` 
+${r===""?"div":r} {
+  width: 100%;
+  overflow-y: scroll;
+  height: 260px;
+}
+
+${r===""?"div":r}::-webkit-scrollbar-thumb:horizontal {
+  width: 4px;
+  background-color: #CCCCCC;
+  -webkit-border-radius: 6px;
+  border-radius: 6px;
+}
+
+${r===""?"div":r}::-webkit-scrollbar-track-piece {
+  background-color: #fff;
+  -webkit-border-radius: 0;
+  border-radius: 0;
+}
+
+${r===""?"div":r}::-webkit-scrollbar {
+  width: 10px;
+  height: 8px;
+}
+
+${r===""?"div":r}::-webkit-scrollbar-thumb:vertical {
+  height: 50px;
+  background-color: var(--bgColor);
+  -webkit-border-radius: 4px;
+  border-radius: 4px;
+  outline: 2px solid #fff;
+  outline-offset: -2px;
+  border: 2px solid #fff;
+}
+
+${r===""?"div":r}::-webkit-scrollbar-thumb:hover {
+  height: 50px;
+  background-color: var(--bgColor);
+  -webkit-border-radius: 4px;
+  border-radius: 4px;
+}
+
+.scrollBar_content {
+  background-color: rgb(162, 171, 209);
+  text-align: center;
+  line-height: 60px;
+  color:rgb(248, 247, 247);
+  margin-bottom: 10px;
+}
+        `,JSCode:()=>"\u6682\u65E0",VueHTMLCode:r=>`<div${r.trim()?` class="${r.replace(".","")}"`:""}>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+  <div class="scrollBar_content">\u5185\u5BB9</div>
+</div>`,VueJSCode:()=>"\u6682\u65E0",ReactHTMLCode:r=>`<div${r.trim()?` className="${r.replace(".","")}"`:""}>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+  <div className="scrollBar_content">\u5185\u5BB9</div>
+</div>`,ReactJSCode:()=>"\u6682\u65E0",TSCode:()=>"\u6682\u65E0",VueTSCode:()=>"\u6682\u65E0",ReactTSCode:()=>"\u6682\u65E0"}})]})]}),p=()=>l("div",{children:[l("div",{className:"component_big_title",children:["\u6EDA\u52A8\u6761 ",c("span",{children:"ScrollBar"})]}),c(u,{})]});export{p as default};
