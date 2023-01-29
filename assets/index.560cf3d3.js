7950386d659cccf5680f8ee7723053653316e98a
LinYan <1439713475@qq.com>
2023-01-29 14:25:54 +0800

    build

---
+++ assets/index.560cf3d3.js
@@ -0,0 +1,80 @@
+import{S as t}from"./index.607cb2d3.js";import{w as i}from"./wrong.13f6fc9b.js";import{a as n,F as r,j as u}from"./index.d546381f.js";/* empty css              */const a=()=>n(r,{children:[u("div",{className:"compontent_title",id:"nav-anchor1",children:"\u57FA\u672C\u5BFC\u822A\u680F"}),u("p",{className:"compontent_p",children:"\u8F83\u4E3A\u57FA\u672C\u7684\u5BFC\u822A\u680F\u3002"}),n("p",{className:"function_p",children:["\u80CC\u666F\u989C\u8272\uFF1A",u("img",{src:i,alt:""})]}),n("p",{className:"function_p",children:["\u5B57\u4F53\u989C\u8272\uFF1A",u("img",{src:i,alt:""})]}),n("p",{className:"function_p",children:["\u60AC\u6D6E\u989C\u8272\uFF1A",u("img",{src:i,alt:""})]}),n("div",{className:"component_box",children:[u("div",{className:"compontent_box_content",children:u("div",{className:"nav_component1_content nav_component1_content_hover",children:u("nav",{children:n("ul",{children:[u("li",{children:u("a",{children:"\u9996\u9875"})}),u("li",{children:u("a",{children:"\u6700\u65B0\u8D44\u8BAF"})}),u("li",{children:u("a",{children:"\u5173\u4E8E\u6211\u4EEC"})})]})})})}),u(t,{code:{element:()=>u("div",{className:"nav_component1_content",children:u("nav",{children:n("ul",{children:[u("li",{children:u("a",{children:"\u9996\u9875"})}),u("li",{children:u("a",{children:"\u6700\u65B0\u8D44\u8BAF"})}),u("li",{children:u("a",{children:"\u5173\u4E8E\u6211\u4EEC"})})]})})}),HTMLCode:e=>`<div${e.trim()?` class="${e.replace(".","")}"`:""}>
+	<nav>
+		<ul>
+			<li>
+				<a href="#">\u9996\u9875</a>
+			</li>
+			<li>
+				<a href="#">\u6700\u65B0\u8D44\u8BAF</a>
+			</li>
+			<li>
+				<a href="#">\u5173\u4E8E\u6211\u4EEC</a>
+			</li>
+		</ul>
+	</nav>
+</div>`,CSSCode:(e,o,l,c)=>`
+${e===""?"div":e} ul {
+	display: flex;
+}
+
+${e===""?"div":e} ul li {
+	cursor: pointer;
+	list-style: none;
+	background-color: ${o};
+	transition: all 0.3s;
+}
+
+${e===""?"div":e} ul li:hover {
+	background-color: ${c};
+}
+
+${e===""?"div":e} ul li a {
+  padding: 0 30px;
+  line-height: 50px;
+  display: inline-block;
+  height: 50px;
+	text-decoration: none;
+	color: ${l};
+	transition: all 0.3s;
+}
+        `,JSCode:()=>"\u6682\u65E0",VueHTMLCode:e=>`<div${e.trim()?` class="${e.replace(".","")}"`:""}>
+	<nav>
+		<ul>
+			<li>
+				<a href="#">\u9996\u9875</a>
+			</li>
+			<li>
+				<a href="#">\u6700\u65B0\u8D44\u8BAF</a>
+			</li>
+			<li>
+				<a href="#">\u5173\u4E8E\u6211\u4EEC</a>
+			</li>
+		</ul>
+	</nav>
+</div>`,VueJSCode:()=>"\u6682\u65E0",ReactHTMLCode:e=>`<div${e.trim()?` className="${e.replace(".","")}"`:""}>
+	<nav>
+		<ul>
+			<li>
+				<a href="#">\u9996\u9875</a>
+			</li>
+			<li>
+				<a href="#">\u6700\u65B0\u8D44\u8BAF</a>
+			</li>
+			<li>
+				<a href="#">\u5173\u4E8E\u6211\u4EEC</a>
+			</li>
+		</ul>
+	</nav>
+</div>`,ReactJSCode:()=>"\u6682\u65E0",TSCode:()=>"\u6682\u65E0",VueTSCode:()=>"\u6682\u65E0",ReactTSCode:()=>"\u6682\u65E0"}})]})]});const h=()=>n(r,{children:[u("div",{className:"compontent_title",id:"button-anchor3",children:"\u57FA\u672C\u5E03\u5C40"}),u("p",{className:"compontent_p",children:"\u5229\u7528margin\u6765\u8FDB\u884C\u5C45\u4E2D,\u5EFA\u8BAE\u81EA\u884C\u8BBE\u7F6E\u5BBD\u5EA6\u3002"}),n("p",{className:"function_p",children:["\u80CC\u666F\u989C\u8272\uFF1A",u("img",{src:i,alt:""})]}),n("p",{className:"function_p",children:["\u5B57\u4F53\u989C\u8272\uFF1A",u("img",{src:i,alt:""})]}),n("p",{className:"function_p",children:["\u60AC\u6D6E\u989C\u8272\uFF1A",u("img",{src:i,alt:""})]}),n("div",{className:"component_box",children:[u("div",{className:"compontent_box_content",children:u("div",{className:"layout_component1_content1",children:"content"})}),u(t,{code:{element:()=>u("div",{className:"layout_component1_content1",children:"content"}),HTMLCode:e=>`<div${e.trim()?` class="${e.replace(".","")}"`:""}>
+  content
+</div>`,CSSCode:(e,o,l,c)=>`${e===""?"div":e} {
+  margin: auto;
+  min-height: 200px;
+  background-color: rgb(225, 227, 238);
+  padding: 10px;
+}
+        `,JSCode:()=>"\u6682\u65E0",VueHTMLCode:e=>`<div${e.trim()?` class="${e.replace(".","")}"`:""}>
+  content
+</div>`,VueJSCode:()=>"\u6682\u65E0",ReactHTMLCode:e=>`<div${e.trim()?` className="${e.replace(".","")}"`:""}>
+  content
+</div>`,ReactJSCode:()=>"\u6682\u65E0",TSCode:()=>"\u6682\u65E0",VueTSCode:()=>"\u6682\u65E0",ReactTSCode:()=>"\u6682\u65E0"}})]})]}),v=()=>n("div",{children:[n("div",{className:"component_big_title",children:["\u5BFC\u822A\u680F ",u("span",{children:"Nav"})]}),u(a,{}),u(h,{})]});export{v as default};
