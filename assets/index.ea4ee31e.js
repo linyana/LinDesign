3e9c7f86c13b150ffb0cbed2ae48437f16045061
LinYan <1439713475@qq.com>
2023-02-01 10:06:46 +0800

    build

---
+++ assets/index.ea4ee31e.js
@@ -0,0 +1,1 @@
+import{c as e,a,s as n,d as t,j as o}from"./index.556a750c.js";const i="/LinDesign/assets/show.4cec0e6a.svg",d=c=>{const s=e();return a("div",{className:"compontent_box_menu",onClick:()=>{s(n(c.code)),s(t())},children:[o("span",{children:"\u67E5\u770B\u4EE3\u7801"}),o("img",{src:i,alt:"\u67E5\u770B\u4EE3\u7801",className:"icon"})]})};export{d as S};
